3db4f4d6cf47dd36d5c83d540161997258579ae4
devfiolaserEric <olv.ericlopes@gmail.com>
2021-12-29 13:46:43 -0300

    first webpage using PHP and HTML.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first webpage</title>
+</head>
+
+<body>
+    <!-- first h1 -->
+    <h1> hello world </h1>
+
+    <?php
+        // security 
+        phpinfo();
+    ?>
+
+</body>
+
+</html>
