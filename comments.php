bc33c780cf52837af52cdae3b1e36bba2a97e2f9
devfiolaserEric <olv.ericlopes@gmail.com>
2021-12-30 10:46:32 -0300

    comments in PHP!

---
+++ comments.php
@@ -0,0 +1,24 @@
+<?php
+
+// this row will be ignored
+
+# this row will be ignored
+
+phpinfo(); // phpinfo # phpinfo
+
+/* block comment
+ *
+ * 
+ * 
+ * echo 123;
+ * 
+ * 
+ */
+
+//  line1
+
+/*
+ line 2
+*/
+
+ echo "test";
