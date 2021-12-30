39fd3460eab8282c42020c98483a6fa224f07c97
devfiolaserEric <olv.ericlopes@gmail.com>
2021-12-30 16:24:54 -0300

    array in php

---
+++ array.php
@@ -0,0 +1,36 @@
+<?php 
+// array lists
+$languages = ["PHP", "Java", "C#"];
+
+echo "programming languages most popular for devs: ";
+
+echo "<br>";
+echo "<br>";
+echo $languages[0];
+echo "<br>";
+echo $languages[1];
+echo "<br>";
+echo $languages[2];
+
+/*
+    position that doesn't exists
+
+    echo "<br>";
+    echo $languages[3];
+
+*/
+
+// another sintax
+
+$languages2 = array("PHP", "C#", "Java");
+echo "<br>";
+
+echo "another way for create array lists";
+
+echo "<br>";
+echo "<br>";
+echo $languages2[0];
+echo "<br>";
+echo $languages2[1];
+echo "<br>";
+echo $languages2[2];
