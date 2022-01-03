2fba6039e34a6affdecab6c26c9178d1e46863e1
devfiolaserEric <olv.ericlopes@gmail.com>
2022-01-03 15:36:33 -0300

    equality and type operators in PHP

---
+++ equalityAndTypeOperators.php
@@ -0,0 +1,20 @@
+<?php
+
+$x = 10;
+$y = "10";
+
+var_dump($x == $y);
+echo "<br>";
+
+// operator identical ===
+var_dump($x === $y);
+echo "<br>";
+
+// types different
+// values equals
+
+// operator not identical !==
+var_dump($x !== $y);
+echo "<br>";
+
+// types and values different
