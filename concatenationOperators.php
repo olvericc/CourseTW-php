5dbab809d5d3f30ef51dc9724c089c4dc0cea2be
devfiolaserEric <olv.ericlopes@gmail.com>
2022-01-03 17:20:06 -0300

    concatenation operators in PHP

---
+++ concatenationOperators.php
@@ -0,0 +1,16 @@
+<?php
+
+$school = "Treinaweb";
+$course = "PHP";
+
+// operator .
+$txt = "course of " . $course. " in school " . $school;
+
+// another way
+/*
+    $txt2 = "course of {$course} in school {$school}";
+*/
+
+echo $txt;
+echo "<br>";
+echo $txt2;
