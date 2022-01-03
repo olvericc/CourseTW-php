01df0d03c755001e77de9345f041cbe6074c471b
devfiolaserEric <olv.ericlopes@gmail.com>
2022-01-03 16:54:19 -0300

    logical operators in PHP

---
+++ logicalOperators.php
@@ -0,0 +1,34 @@
+<?php
+
+$v1 = true;
+$v2 = false;
+
+// operador && and
+
+var_dump($v1 && $v2);
+echo "<br>";
+
+// another way
+/*
+    var_dump($v1 and $v2);
+    echo "<br>";
+*/
+
+// operator || or 
+
+var_dump($v1 || $v2);
+echo "<br>";
+
+// another way
+/*
+    var_dump($v1 or $v2);
+    echo "<br>";
+*/
+
+// operator xor
+var_dump($v1 xor $v2);
+echo "<br>";
+
+// operator not !
+var_dump(!$v2);
+echo "<br>";
