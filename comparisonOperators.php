78f3b4a1e2dc0dc804f8a2136ae6162a7026338b
devfiolaserEric <olv.ericlopes@gmail.com>
2022-01-03 15:15:38 -0300

    comparison operators in PHP

---
+++ comparisonOperators.php
@@ -0,0 +1,38 @@
+<?php
+
+// comparison
+
+$a = 10;
+$b = 20;
+
+// operator ==
+var_dump($a == $b);
+echo "<br>";
+
+// operator <> or !=
+var_dump($a <> $b); # or use var_dump($a != $b);
+echo "<br>";
+
+// operator >
+var_dump($a > $b);
+echo "<br>";
+
+// operator <
+var_dump($a < $b);
+echo "<br>";
+
+// operator >=
+var_dump($a >= $b);
+echo "<br>";
+
+// operator <=
+var_dump($a <= $b);
+echo "<br>";
+
+// operator ===
+var_dump($a === $b);
+echo "<br>";
+
+// operator !==
+var_dump($a !== $b);
+echo "<br>";
