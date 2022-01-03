7d2e6d659115ec46057db99f77344235fdfc6c41
devfiolaserEric <olv.ericlopes@gmail.com>
2022-01-03 14:29:53 -0300

    arithmetics operators in PHP

---
+++ arithmeticsOperators.php
@@ -0,0 +1,52 @@
+<?php
+
+// operator +
+$sum = 5 + 6.6;
+echo ("sum: {$sum}");
+echo "<br>";
+
+// operator -
+$subtraction = 8.4 - 1;
+echo ("subtraction: {$subtraction}");
+echo "<br>";
+
+// operator *
+$multiplication = 5.9 * 2;
+echo ("multiplication: {$multiplication}");
+echo "<br>";
+
+// operator /
+$division = 100 / 6;
+echo ("division: {$division}");
+echo "<br>";
+
+// operator % (mod)
+$mod = 50 % 3;
+echo ("mod: {$mod}");
+echo "<br>";
+
+// operator ** (exponential)
+$exponential = 2 ** 3;
+echo ("exponential: {$exponential}");
+echo "<br>";
+
+echo "--------------------------------------<br>";
+
+// var_dumps
+var_dump($sum);
+echo "<br>";
+
+var_dump($subtraction);
+echo "<br>";
+
+var_dump($multiplication);
+echo "<br>";
+
+var_dump($division);
+echo "<br>";
+
+var_dump($mod);
+echo "<br>";
+
+var_dump($exponential);
+echo "<br>";
