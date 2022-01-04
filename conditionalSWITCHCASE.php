657572ccd58d8bb8ee9936338d0bba408e1a5e60
devfiolaserEric <olv.ericlopes@gmail.com>
2022-01-04 12:04:06 -0300

    conditional switch-case in PHP

---
+++ conditionalSWITCHCASE.php
@@ -0,0 +1,30 @@
+<?php
+
+$daysOfWeek = 7;
+
+switch ($daysOfWeek) {
+    case 1:
+        echo "sunday";
+        break;
+    case 2:
+        echo "monday";
+        break;
+    case 3: 
+        echo "tuesday";
+        break;
+    case 4:
+        echo "wednesday";
+        break;
+    case 5:
+        echo "thursday";
+        break;
+    case 6: 
+        echo "friday";
+        break;
+    case 7:
+        echo "saturday";
+        break;
+    default:
+        echo "error";
+        break;
+}
