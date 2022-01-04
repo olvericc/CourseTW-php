baabd7f315dbaaed38963914717da797d199490b
devfiolaserEric <olv.ericlopes@gmail.com>
2022-01-04 11:38:35 -0300

    conditional elseif and else in PHP

---
+++ conditionalELSEIF.php
@@ -0,0 +1,14 @@
+<?php
+
+$value1 = 10;
+$value2 = 20;
+
+if ($value1 > $value2){
+    echo "value1 bigger than value2 <br>";
+} elseif ($value2 == 20){
+    echo "value 2 equals 20 <br>";
+} elseif ($value1 == 10){
+    echo "value 1 equals 10 <br>";
+} else {
+    echo "value 1 and value 2 be different";
+}
