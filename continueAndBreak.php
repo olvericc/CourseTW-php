c3e250627705c2e6cf03008956d6773953b94cff
devfiolaserEric <olv.ericlopes@gmail.com>
2022-01-11 16:31:09 -0300

    continue and break in PHP

---
+++ continueAndBreak.php
@@ -0,0 +1,47 @@
+<?php
+
+// associative array example
+$courses = [
+    "php" => [
+        "course_name" => "Course of PHP fundamentals",
+        "tool_version" => 7.4,
+        "workload" => 40,
+        "status" => true
+    ],
+    "java" => [
+        "course_name" => "Course of Java fundamentals",
+        "tool_version" => 11.4,
+        "workload" => 30,
+        "status" => false
+    ],
+    "c#" => [
+        "course_name" => "Course of C# fundamentals",
+        "tool_version" => 9.0,
+        "workload" => 55,
+        "status" => true
+    ]
+];
+
+foreach ($courses as $specificCourse) {
+    // using continue
+    /*
+        if ($specificCourse['status' == false]) {
+            continue;
+        }
+    */
+    
+    // using break
+    if ($specificCourse['status' == false]) {
+        break;
+    }
+
+    echo "------------------------------------------------ <br>";
+    echo "course name: " . $specificCourse['course_name'];
+    echo "<br>";
+    echo "tool version: " . $specificCourse['tool_version'];
+    echo "<br>";
+    echo "workload: " . $specificCourse['workload'];
+    echo "<br>";
+    echo "status: " . $specificCourse['status'];
+    echo "<br>";
+}
